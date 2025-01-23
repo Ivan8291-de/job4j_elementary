f03e4cbc7a445eb25c010c64c72aeee7475f4bbd
origin <git@github.com:OljaKn/job4j_design.git>
2025-01-23 21:55:48 +0300

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -12,7 +12,17 @@ public class Converter {
     public static void main(String[] args) {
         float euro = rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
+        float input = 140;
+        float expected = 2;
+        float output = Converter.rubleToEuro(input);
+        boolean passed = expected == output;
+        System.out.println("140 rubles are 2. Test result : " + passed);
         float dollar = rubleToDollar(240);
         System.out.println("240 rubles are " + dollar + " dollar.");
+        float input1 = 240;
+        float expected1 = 4;
+        float output1 = Converter.rubleToDollar(input1);
+        boolean passed1 = expected1 == output1;
+        System.out.println("240 rubles are 4. Test result : " + passed1);
     }
 }
