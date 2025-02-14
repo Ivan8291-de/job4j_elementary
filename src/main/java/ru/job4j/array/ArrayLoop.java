02113801f5be314c77e007b618b07f586a1da53e
origin <git@github.com:OljaKn/job4j_design.git>
2025-02-14 15:42:45 +0300

    6.0.4. Массивы и цикл for. [#226665 #90544]

---
+++ src/main/java/ru/job4j/array/ArrayLoop.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+public class ArrayLoop {
+    public static void main(String[] args) {
+        int[] a = new int[5];
+        for (int index = 0; index < a.length; index++) {
+            a[index] = index * 2 + 3;
+        }
+        for (int index = 0; index < a.length; index++) {
+            System.out.println(a[index]);
+        }
+    }
+}
