e3bc4122261b542c23c0c7e33eb0e7477b3b5a06
origin <git@github.com:OljaKn/job4j_design.git>
2025-02-14 15:22:50 +0300

    6.0.3. Заполнение массива. [#226300 #90574]

--- src/main/java/ru/job4j/array/Definition.java
+++ src/main/java/ru/job4j/array/Definition.java
@@ -7,6 +7,11 @@ public class Definition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Ivan Dorofeev";
+        names[1] = "Olga Knyazeva";
+        names[2] = " Mary Knyazeva";
+        names[3] = "Mikhail Knyazev";
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
