eee5a51d2dce2570b3676286598103a01debecb7
origin <git@github.com:OljaKn/job4j_design.git>
2025-03-02 13:06:41 +0300

    6.7.4. Выигрышные комбинации в сокобан [#53859 #90568]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -31,4 +31,21 @@ public class MatrixCheck {
         }
         return result;
     }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        int j = 0;
+        for (int i = 0; i < board.length; i++) {
+            j = i;
+            if (board[i][j] == 'X') {
+                boolean horizontal = monoHorizontal(board, i);
+                boolean vertical = monoVertical(board, j);
+                if (horizontal || vertical) {
+                    result = true;
+                }
+                break;
+            }
+        }
+        return result;
+    }
 }
