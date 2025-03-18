53a792239ed106c03e665cd1a5f4ee176d3ccc70
origin <git@github.com:OljaKn/job4j_design.git>
2025-03-18 22:15:52 +0300

    3. Тестовое задание. Блок "Базовый синтаксис" [#505049 #503200]

---
+++ src/main/java/ru/job4j/array/SimpleStringEncoder.java
@@ -0,0 +1,31 @@
+package ru.job4j.array;
+
+public class SimpleStringEncoder {
+    public static String encode(String input) {
+        if (input.isEmpty()) {
+            return "";
+        }
+        String result = "";
+        char symbol = input.charAt(0);
+        int counter = 1;
+
+        for (int i = 1; i < input.length(); i++) {
+            if (input.charAt(i) == symbol) {
+                counter += 1;
+            } else {
+                result += symbol;
+                if (counter > 1) {
+                    result += counter;
+                }
+                symbol = input.charAt(i);
+                counter = 1;
+            }
+        }
+        result += symbol;
+        if (counter > 1) {
+            result += counter;
+        }
+        return result;
+
+    }
+}
